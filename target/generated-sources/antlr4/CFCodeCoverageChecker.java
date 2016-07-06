9207b65749391061bf5c4f819dd793e5f9e461ed
rmarchandTen24 <ryan.marchand@ten24web.com>
2016-07-06 04:56:23 -0400

    able to read directory from command line args

--- target/generated-sources/antlr4/CFCodeCoverageChecker.java
+++ target/generated-sources/antlr4/CFCodeCoverageChecker.java
@@ -1,8 +1,14 @@
 
 public class CFCodeCoverageChecker extends CFSCRIPTParserBaseListener {
+	
 	public static void main(String[] args) {
 		System.out.println("This is the main section ");
+		
+		
 		//STEP 1: be able to pass Slatwall directory as an argument from command line
+		
+		String slatwallDirectoryPath = args[0];
+		System.out.println( slatwallDirectoryPath  );
 		
 		//STEP 2: read all file names in /model folder store info in an array of file objects 
 		//		  [
