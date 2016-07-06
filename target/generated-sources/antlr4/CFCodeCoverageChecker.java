73ba2d8aacc53d1636414181c30273cdb3ccc9ea
rmarchandTen24 <ryan.marchand@ten24web.com>
2016-07-06 04:44:17 -0400

    updating psuedo code

--- target/generated-sources/antlr4/CFCodeCoverageChecker.java
+++ target/generated-sources/antlr4/CFCodeCoverageChecker.java
@@ -2,7 +2,7 @@
 public class CFCodeCoverageChecker extends CFSCRIPTParserBaseListener {
 	public static void main(String[] args) {
 		System.out.println("This is the main section ");
-		//STEP 1: be able to pass Slatwall directory as an argument
+		//STEP 1: be able to pass Slatwall directory as an argument from command line
 		
 		//STEP 2: read all file names in /model folder store info in an array of file objects 
 		//		  [
