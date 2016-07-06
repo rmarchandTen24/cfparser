67e03fd5f64a679923523df70c65e4cc45003a6d
rmarchandTen24 <ryan.marchand@ten24web.com>
2016-07-06 04:42:56 -0400

    pseudo code

--- target/generated-sources/antlr4/CFCodeCoverageChecker.java
+++ target/generated-sources/antlr4/CFCodeCoverageChecker.java
@@ -1,6 +1,22 @@
 
 public class CFCodeCoverageChecker extends CFSCRIPTParserBaseListener {
 	public static void main(String[] args) {
-		System.out.println("Enter a password: ");
+		System.out.println("This is the main section ");
+		//STEP 1: be able to pass Slatwall directory as an argument
+		
+		//STEP 2: read all file names in /model folder store info in an array of file objects 
+		//		  [
+		//			{
+		//				filename:"Product.cfc",
+		//				path:"/entity"
+		//				functions:["getAvailableForPurchaseFlag","..."]
+		//			}
+		//		  ] 
+		
+		//STEP 3: check for equivalent test files in /meta/tests/unit{{file.path}}/{{file.filename}}
+		//Store in an array of testfiles with keys filename and path as well.
+		
+		//STEP 4: parse test files and verify function is being tested
+		
 	}
 }
